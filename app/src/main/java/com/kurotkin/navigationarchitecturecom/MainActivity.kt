04a291b3e4a5d24be71fe4ebfdedd1d4ffee7cde
Vitaly Kurotkin <example@example.com>
2020-07-23 15:56:00 +0300

    MyObserver

--- app/src/main/java/com/kurotkin/navigationarchitecturecom/MainActivity.kt
+++ app/src/main/java/com/kurotkin/navigationarchitecturecom/MainActivity.kt
@@ -2,7 +2,11 @@ package com.kurotkin.navigationarchitecturecom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,10 +15,14 @@ import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration : AppBarConfiguration
+    lateinit var myObserver: MyObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myObserver = MyObserver()
+        lifecycle.addObserver(myObserver)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
---
+++ app/src/main/java/com/kurotkin/navigationarchitecturecom/MyObserver.kt
@@ -0,0 +1,15 @@
+package com.kurotkin.navigationarchitecturecom
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+
+class MyObserver : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun onAny(source: LifecycleOwner, event: Lifecycle.Event) {
+        Log.d("TAG", event.toString())
+    }
+}
